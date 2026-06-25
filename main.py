cd85371af1e5e22f6b329f4e9e1bb950efcd888a
Pepemi81 <pepemimolina@gmail.com>
2026-06-25 20:54:26 +0200

    Main cleanup

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
+print("--------------------------------------- \n")
 
 try:
     dataset_url="https://atlantis.sci.utah.edu/mod_visus?dataset=nex-gddp-cmip6"
@@ -13,16 +14,16 @@ except:
 
 
 model     = "ACCESS-CM2"
-variable  = "tas" 
+variable  = "pr" #pr: precipitacion tas: temp
 scenario  = "historical"
-timestamp = '2012-01-10'
+timestamp = '2010-04-20' #aaaa-mm-dd
 
 field = f"{variable}_day_{model}_{scenario}_r1i1p1f1_gn"
 
 def calculate_day_of_year(date_str):
     date = datetime.strptime(date_str, '%Y-%m-%d')
     start_of_year = datetime(date.year, 1, 1)
-    day_of_year = (date - start_of_year).days   
+    day_of_year = (date - start_of_year).days
     return day_of_year
 
 def get_timestep(date_str):
@@ -35,39 +36,49 @@ timestep_index=int(get_timestep(timestamp))
 #print(timestep_index)
 
 db=ov.LoadDataset(dataset_url)
-#print(f'Dimensions: {db.getLogicBox()[1][0]}*{db.getLogicBox()[1][1]}')
-#print(f'Total Timesteps: {len(db.getTimesteps())}')
+print(f'Dimensions: {db.getLogicBox()[1][0]}*{db.getLogicBox()[1][1]}')
+print(f'Total Timesteps: {len(db.getTimesteps())}')
+
+
 #print(f'Field: {field}')
 #print('Data Type: float32')
-
 data=db.read(time=timestep_index,quality=0,field=field)
 data.shape
 
 
 #visualizacion
 
-y_max, x_max = data.shape
-xlabels = [str(x) for x in range(0, 360, 50)]
-ylabels = [str(y) for y in range(-60, 90, 20)]
-xticks = np.linspace(0, x_max, len(xlabels))
-yticks = np.linspace(0, y_max, len(ylabels))
 
-# Create the plot
-fig, axes = plt.subplots(1, 1,figsize=(10, 8))
-axes.set_xticks(xticks)
-axes.set_xticklabels(xlabels)
-axes.set_yticks(yticks)
-axes.set_yticklabels(ylabels)
+def test_visualization():
+    
+    y_max, x_max = data.shape
+    xlabels = [str(x) for x in range(0, 360, 50)]
+    ylabels = [str(y) for y in range(-60, 90, 20)]
+    xticks = np.linspace(0, x_max, len(xlabels))
+    yticks = np.linspace(0, y_max, len(ylabels))
 
-# Plot the data
-im = axes.imshow(data[:, :], origin='lower', cmap='turbo')
+    # Create the plot
+    fig, axes = plt.subplots(1, 1,figsize=(10, 8))
+    axes.set_xticks(xticks)
+    axes.set_xticklabels(xlabels)
+    axes.set_yticks(yticks)
+    axes.set_yticklabels(ylabels)
 
-divider = make_axes_locatable(axes)
-cax = divider.append_axes("right", size="5%", pad=0.1)  # Adjust size and pad as needed
+    # Plot the data
+    im = axes.imshow(data[:, :], origin='lower', cmap='turbo')
 
-# Add the colorbar
-cbar = plt.colorbar(im, cax=cax)
-cbar.set_label('Precipitation (kg m-2 s-1)')
+    divider = make_axes_locatable(axes)
+    cax = divider.append_axes("right", size="5%", pad=0.1)  # Adjust size and pad as needed
 
-# Show the plot
-plt.show()
+    # Add the colorbar
+    cbar = plt.colorbar(im, cax=cax)
+    cbar.set_label('Precipitation (kg m-2 s-1)')
+
+    # Show the plot
+    plt.show()
+
+
+test_visualization()
+
+
+print("---------------------------------------")
