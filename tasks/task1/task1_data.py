dca4bb394317b6767074b3aeceefd3811c8eb5d1
hjaviervalesc <hjavier.valesc@gmail.com>
2026-07-05 13:35:23 +0200

    Task1 acabada, sin changelog

--- tasks/task1/task1_data.py
+++ tasks/task1/task1_data.py
@@ -2,66 +2,39 @@ import OpenVisus as ov
 from datetime import datetime, timedelta
 
 # ----- Config -----
+MAX_TIMESTEP = 10268
+MAX_FACES = 5
 
-MIN_DATE = "1950-01-01"
-MAX_DATE = "2011-12-31"
+START_DATE = "2020-01-20"
+HOURS_PER_TIMESTEP = 1  # cada timestep = 1 hora
 
-# Dias totales reales entre 1950-01-01 y 2011-12-31
-MAX_TIMESTEP = 22644
-
-# ----- Time -----
-
-def get_timestep(date_str):
-    """
-    Calcula el índice específico del servidor OpenVisus para una fecha dada.
-    
-    Args:
-        date_str (str): Fecha objetivo en formato 'AAAA-MM-DD'.
-        
-    Returns:
-        int: El índice absoluto de timestep requerido por la base de datos de OpenVisus.
-    """
-    date_obj = datetime.strptime(date_str, '%Y-%m-%d')
-    start_of_year = datetime(date_obj.year, 1, 1)
-    
-    day_of_year = (date_obj - start_of_year).days
-    
-    is_leap_year = False
-    if date_obj.year % 4 == 0:
-        if date_obj.year % 100 != 0 or date_obj.year % 400 == 0:
-            is_leap_year = True
-            
-    if is_leap_year == True:
-        total_days_in_year = 366
-    else:
-        total_days_in_year = 365
-        
-    timestep_index = (date_obj.year * total_days_in_year) + day_of_year
-    
-    return int(timestep_index)
+# ----- Date -----
 
 def get_date_by_timestep(timestep):
     """
-    Convierte un timestep lineal normalizado de vuelta a una cadena de fecha estándar.
-    
+    Convierte un timestep del experimento a una fecha completa con
+    año, mes, día, hora, minuto y segundo.
+
     Args:
-        timestep (int): Paso lineal de 0 a 22644 (días desde 1950-01-01).
-        
+        timestep (int): índice de timestep (0, 1, 2, ...)
+
     Returns:
-        str: La fecha calculada en formato 'AAAA-MM-DD'.
+        str: Fecha y hora en formato 'AAAA-MM-DD HH:MM:SS'
     """
-    base_date = datetime.strptime(MIN_DATE, "%Y-%m-%d")
-    target_date = base_date + timedelta(days=int(timestep))
-    return target_date.strftime("%Y-%m-%d")
+    base_date = datetime.strptime(START_DATE, "%Y-%m-%d")
+    delta = timedelta(hours=timestep * HOURS_PER_TIMESTEP)
+    target_date = base_date + delta
+    return target_date.strftime("%Y-%m-%d %H:%M:%S")
+
 
 # ----- Data -----
 
-def get_data_by_date(variable, face, date_str, z):
+def get_data_by_timestep(variable, face, timestep, z):
     """
-    Descarga una matriz 2D de datos climáticos desde el servidor usando una fecha específica.
+    Descarga un bloque 3D de datos climáticos para una variable, una face del cubed‑sphere y un rango vertical Z en una fecha específica.
     
     Args:
-        variable (str): Variable climática a consultar (ej. 'U' (eastward wind), 'V' (northward wind)).
+        variable (str): Variable climática a consultar (ej. 'u' (eastward wind), 'v' (northward wind)).
         face (str): región de la Tierra a consultar (Válido:'0', '1', '2', '3', '4', '5').
             GEOS utiliza una malla cubed‑sphere, donde la superficie terrestre se divide en 6 regiones llamadas faces. 
             Cada face corresponde a la proyección de una cara del cubo sobre la esfera. 
@@ -73,35 +46,36 @@ def get_data_by_date(variable, face, date_str, z):
                 -Face 4: Índico 
                 -Face 5: la región polar
         date_str (str): Fecha objetivo en formato 'AAAA-MM-DD' (Válido: 1950-01-01 a 2011-12-31).
-        z (list): Lista de índices de diferentes niveles de profundidad (z) a consultar en formato [inicio,fin] (Válido: [0,51]).
-        
+        z (list): Lista de índices de diferentes niveles de profundidad (z) a consultar en formato [inicio,fin] (Válido: [0, 51])(Ej: [3,4], [30,35], [45,51]).
+                  A más grande el intervalo más tiempo de procesamiento.
     Returns:
-        numpy.ndarray: La matriz 2D que contiene los datos climáticos solicitados.
+        numpy.ndarray: Volumen 3D (latitud × longitud × depth (z)) con los datos climáticos solicitados.
     """    
-    geos_face_loc=f"https://nsdf-climate3-origin.nationalresearchplatform.org:50098/nasa/nsdf/climate3/dyamond/GEOS/GEOS_{variable.upper()}/{variable.lower()}_face_{face}_depth_52_time_0_10269.idx"
-    db=ov.LoadDataset(geos_face_loc)
-    timestep = get_timestep(date_str)
-    data = db.read(time=timestep, quality=0, z=z)
-    
-    return data
-
-def get_data_by_timestep(variable, timestep):
-    """
-    Descarga una matriz 2D de datos climáticos usando un índice de timestep lineal.
-    Ideal para controles deslizantes (sliders) de interfaz de usuario o bucles de iteración.
-    
-    Args:
-        variable (str): Variable climática a consultar (ej. 'tas', 'pr').
-        timestep (int): Índice lineal entre 0 y 22644.
-        
-    Returns:
-        numpy.ndarray: La matriz 2D que contiene los datos climáticos solicitados.
-    """
     if timestep < 0 or timestep > MAX_TIMESTEP:
         print(f"\n[WARNING] Timestep {timestep} is out of bounds.")
         print(f"[WARNING] Valid range: 0 to {MAX_TIMESTEP}")
-        
-    date = get_date_by_timestep(timestep)
-    data = get_data_by_date(variable, date)
+    if face < 0 or face > MAX_FACES:
+        print(f"\n[WARNING] Face {face} is out of bounds.")
+        print(f"[WARNING] Valid range: 0 to {MAX_FACES}")
+    if not isinstance(z, (list, tuple)) or len(z) != 2:
+        print("\n[WARNING] z must be a list of two integers: [start, end]")
+    else:
+        z_start, z_end = z
+
+        if z_start < 0 or z_start > 51:
+            print(f"\n[WARNING] z start {z_start} is out of bounds.")
+            print("[WARNING] Valid range: 0 to 51")
+
+        if z_end < 0 or z_end > 51:
+            print(f"\n[WARNING] z end {z_end} is out of bounds.")
+            print("[WARNING] Valid range: 0 to 51")
+
+        if z_start >= z_end:
+            print(f"\n[WARNING] Invalid z interval: [{z_start}, {z_end}]")
+            print("[WARNING] z_inicio tiene que ser menor a z_final")
+
+    geos_face_loc=f"https://nsdf-climate3-origin.nationalresearchplatform.org:50098/nasa/nsdf/climate3/dyamond/GEOS/GEOS_{variable.upper()}/{variable.lower()}_face_{face}_depth_52_time_0_10269.idx"
+    db=ov.LoadDataset(geos_face_loc)
+    data = db.read(time=timestep, quality=0, z=z)
     
     return data
--- tasks/task1/visualize_by_timestep.py
+++ tasks/task1/visualize_by_timestep.py
@@ -1,17 +1,26 @@
 import matplotlib.pyplot as plt
-import task0_data
+import task1_data
 
-target_variable = "pr" 
-target_timestep = 12000
+target_variable = "u" 
+target_timestep = 10268
+target_face = 5
+target_z = [0, 2]  
 
 print(f"Downloading data for '{target_variable}' at timestep {target_timestep}...")
 
-climate_data = task0_data.get_data_by_timestep(target_variable, target_timestep)
-real_date = task0_data.get_date_by_timestep(target_timestep)
+climate_data = task1_data.get_data_by_timestep(target_variable, target_face, target_timestep, target_z)
+real_date = task1_data.get_date_by_timestep(target_timestep)
 
-fig, ax = plt.subplots(figsize=(10, 6))
-im = ax.imshow(climate_data, origin='lower', cmap='turbo')
 
-ax.set_title(f"CMIP6 ACCESS-CM2 - {target_variable.upper()} (Timestep: {target_timestep} | Date: {real_date})")
-plt.colorbar(im, ax=ax, label="Variable Units")
+fig, axes = plt.subplots(1, 1, figsize=(10, 8))
+
+im = axes.imshow(climate_data[0, :, :], aspect='auto', origin='lower', cmap='viridis')
+cbar = plt.colorbar(im, ax=axes)
+cbar.set_label('East-west wind speed (m/s)')
+
+axes.set_title(
+    f"DYAMOND GEOS - {target_variable.upper()} "
+    f"(Timestep: {target_timestep} | Date: {real_date})"
+)
+
 plt.show()
